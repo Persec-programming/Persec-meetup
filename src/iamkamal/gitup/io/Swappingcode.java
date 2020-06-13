4bcfaf3bee9917407c1ec5a7d0b727b8fd419423
kamalrathod <kamalathod23@gmail.com>
2020-06-14 00:04:20 +0530

    this is my second program swapping

---
+++ src/iamkamal/gitup/io/Swappingcode.java
@@ -0,0 +1,20 @@
+package iamkamal.gitup.io;
+
+import java.util.Scanner;
+
+public class Swappingcode {
+    public static void main (String[] args)
+{
+    int a,b,c;
+    Scanner kb= new Scanner(System.in);
+    System.out.println("Enter the value of A");
+     a = kb.nextInt();// kb= new Scanner(System.in);
+    System.out.println("Enter the value of B");
+    b = kb.nextInt();
+    //c==b;
+    System.out.println("the value of A"+b);
+    System.out.println("the value of B"+a);
+}
+
+
+}
