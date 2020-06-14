e53053702152fe5785b9bf627aad1d5996bf6413
iamrajendra <rajendrarajaram@hotmail.com>
2020-06-14 11:46:54 +0530

    Create a class,Objects,Arrtibutes

---
+++ src/iamrajendra/github/io/obs/A.java
@@ -0,0 +1,8 @@
+package iamrajendra.github.io.classes;
+
+public class A {
+    int x = 5;
+public String  helloWord(){
+    return "I am a method in side a class A";
+}
+}
---
+++ src/iamrajendra/github/io/obs/Example1.java
@@ -0,0 +1,15 @@
+package iamrajendra.github.io.classes;
+
+import java.util.Arrays;
+
+public class Example1 {
+    private static A a;
+
+    public static void main(String[] args) {
+        a = new A();
+        System.out.println("Value of X " + a.x);
+        System.out.println( a.helloWord());
+    }
+
+
+}
