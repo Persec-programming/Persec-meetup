d9024f5e23ad3fd96cefbebb929775553a72e0a2
aweshkhan <awesh.persec@gmail.com>
2020-06-21 16:06:15 +0530

    this is my project 2 helloZZZZZZZZZZZZ
    exit

---
+++ src/iamAwesh/github/io/SwapingTwoNum.java
@@ -0,0 +1,18 @@
+package iamAwesh.github.io;
+
+import java.util.Scanner;
+
+public class SwapingTwoNum {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner AK= new Scanner(System.in);
+        System.out.println("Enter the value of A");
+        a = AK.nextInt();// kb= new Scanner(System.in);
+        System.out.println("Enter the value of B");
+        b = AK.nextInt();
+        //c==b;
+        System.out.println("the value of A"+b);
+        System.out.println("the value of B"+a);
+    }
+}
+
